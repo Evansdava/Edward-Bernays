17570552bf532484e8144fd852dba353eeda0515
David Evans <evansdav.a@gmail.com>
2019-11-05 15:26:28 -0800

    Added career and methods pages, began filling in information

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/career')
+def career():
+    return render_template('career.html')
+
+
+@app.route('/methods')
+def methods():
+    return render_template('methods.html')
